7a570bdc247a75c09adefc9218bc629cd2cbb157
Kevin Roney <ktroney.pdx@gmail.com>
2022-04-11 14:43:43 -0700

    added event listener to business signup page

--- business/business.js
+++ business/business.js
@@ -2,11 +2,14 @@ import { checkAuth,
     logout, 
     createBusiness,
     createAdmin,
-    signupUser } from '../fetch-utils.js';
+    signupUser,
+    redirectIfLoggedIn } from '../fetch-utils.js';
 
 // checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const businessSignUpForm = document.querySelector('form');
+
 
 logoutButton.addEventListener('click', () => {
     logout();
@@ -14,11 +17,30 @@ logoutButton.addEventListener('click', () => {
 
 businessSignUpForm.addEventListener('submit', async (event) => {
     event.preventDefault();
-    const user = await signupUser(signUpEmail.value, signUpPassword.value);
+    const data = new FormData(businessSignUpForm);
+    const user = await signupUser(data.email, data.password);
+    const code = Math.ceil(Math.random() * 99999);
+    const bizName = data.get('bizName');
+    const admin = data.get('adminName');
+    const email = data.get('email');
+    const password = data.get('password');
+    await createBusiness({
+        name: bizName,
+        business_code: code
 
+    });
+    await signupUser(email, password);
+    await createAdmin({
+        name: admin,
+        business_code: code,
+        avatar: data.file,
+        email: email,
+    });
     if (user) {
         redirectIfLoggedIn();
     } else {
         console.error(user);
     }
+    console.log(code);
+    businessSignUpForm.reset();
 });

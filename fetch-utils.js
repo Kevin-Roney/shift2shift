d8a6262dc9a75aa50ef2a9dff0e1bf472aa5e6b0
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-04-11 11:56:59 -0700

    created createAdmin and createBusiness funcs in fetch-utils.js"

--- fetch-utils.js
+++ fetch-utils.js
@@ -3,6 +3,35 @@ const SUPABASE_KEY = '';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+//Takes business name input from signup form and inserts name and random generated code into businesses table on supabase
+export async function createBusiness(business) {
+    const response = await client
+        .from('businesses')
+        .insert({
+            name: business.name,
+            business_code: business.business_code
+        });
+    
+    return checkError(response);
+}
+
+//takes businesses signup information and generates admin to employee table
+export async function createAdmin(employee) {
+    const response = await client
+        .from('employees')
+        .insert({
+            name: employee.name,
+            business_code: employee.business_code,
+            shift: 3,
+            is_admin: true,
+            avatar: employee.avatar_img,
+            email: employee.email,
+            user_id: employee.user_id
+        });
+
+    return checkError(response);
+}
+
 export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
@@ -37,6 +66,6 @@ export async function logout() {
     return (window.location.href = '../');
 }
 
-// function checkError({ data, error }) {
-//     return error ? console.error(error) : data;
-// }
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}

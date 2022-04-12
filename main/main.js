56882715dce26a341ad2d21d8ad3088630376384
SusanBird <susanbird391@gmail.com>
2022-04-12 12:44:42 -0700

    todosForm eventlistener

--- main/main.js
+++ main/main.js
@@ -1,4 +1,4 @@
-import { logout, checkAuth } from '../fetch-utils.js';
+import { logout, createTodo, getEmployee } from '../fetch-utils.js';
 
 const logoutButton = document.querySelector('#logout');
 
@@ -11,4 +11,22 @@ logoutButton.addEventListener('click', async () => {
 
 const todosEl = document.querySelector('.todosContainer');
 const shiftNotesEl = document.querySelector('.shiftNotesContainer');
+const todosForm = document.querySelector('#todoListForm');
 
+todosForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(todosForm);
+    const todoObj = data.get('todo');
+    const urgencyObj = data.get('urgency');
+    const user = await getEmployee();
+    const business_code = user.business_code;
+
+    await createTodo({
+        todo_name: todoObj,
+        urgency: urgencyObj,
+        business_code: business_code,
+    });
+
+    todosForm.reset();
+});
